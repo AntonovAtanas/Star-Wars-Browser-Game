6b8d3c2c4b45f027316972721bd9645e79a7313f
AntonovAtanas <atanas.antonov1@gmail.com>
2023-02-19 22:27:00 +0200

    Adding the Millenium Falcon after starting the game

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-const startBtn = document.querySelector('.startScreen')
+const startBtn = document.querySelector('.start-screen')
 startBtn.addEventListener('click', start);
+
+const gameScreen = document.querySelector('.game-screen');
 
 function start(){
     startBtn.classList.add('hidden');
 
-    
+    let milleniumFalcon = document.createElement('div');
+    milleniumFalcon.classList.add('millenium-falcon');
+    gameScreen.appendChild(milleniumFalcon);
+
 }
 
