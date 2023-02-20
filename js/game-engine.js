a291fece508aa70b6cd26dc2ad9eed119d01fc41
AntonovAtanas <atanas.antonov1@gmail.com>
2023-02-20 19:26:01 +0200

    Added movement to the Millenium Falcon in all directions

--- js/game-engine.js
+++ js/game-engine.js
@@ -7,15 +7,27 @@ function gameLoop(state, game) {
     // Movement of the Millenium Falcon
     if (state.keys['ArrowDown']) {
         state.milleniumFalcon.positionTop += 5;
-    }
+    };
     
     if (state.keys['ArrowUp']) {
         state.milleniumFalcon.positionTop -= 5;
-    }
+    };
+
+    if (state.keys['ArrowRight']) {
+        state.milleniumFalcon.positionLeft += 5;
+    };
+
+    if (state.keys['ArrowLeft']) {
+        state.milleniumFalcon.positionLeft -= 5;
+    };
+
+
 
     //Rendering
     game.milleniumFalcon.style.top = state.milleniumFalcon.positionTop + 'px';
     game.milleniumFalcon.style.bottom = state.milleniumFalcon.positionDown + 'px';
+    game.milleniumFalcon.style.left = state.milleniumFalcon.positionLeft + 'px';
+    game.milleniumFalcon.style.right = state.milleniumFalcon.positionLeft + 'px';
 
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 };
